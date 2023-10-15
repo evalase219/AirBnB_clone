44c7847aad4483a1dbb3de4ba3ce268a542ff68c
Eric Acquah <ericacquah1720@gmail.com>
2023-10-15 22:17:18 +0000

    Console 70% done

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.city import City
 from models.state import State
+from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 from models.__init__ import storage
